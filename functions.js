21336267fe8e7b2b1831e0c73d86cd568c0f5cdf
Ovidiu Gusteritan <gusteritanovidiu@gmail.com>
2024-09-02 16:21:50 +0300

    done alerts

--- functions.js
+++ functions.js
@@ -1,10 +1,14 @@
-export function createHome() {
+export function createHome(alert) {
 
     let container = document.querySelector(".container");
 
 
     container.innerHTML = `
     
+ <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+       </div>  
+
     	<h1>Products</h1>
 
     <button class="button">Add product</button>
@@ -23,22 +27,77 @@ export function createHome() {
 		</tbody>
 	</table>
     `
+    let button = document.querySelector(".button");
+    let table = document.querySelector(".table");
+    const alertPlaceholder = document.querySelector('.container-alert');
+    let load = document.querySelector(".spinner-border");
+
+    const appendAlert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
+    }
 
     api("https://localhost:7040/api/v1/Product/all").then(response => {
         return response.json();
     }).then(data => {
+        load.classList = "";
         console.log(data);
         attachProducts(data.productList);
     }).catch(error => {
+        load.classList = "";
         console.error('Error fetching data:', error);
+        appendAlert(error, "danger");
     });
-
-
-    let button = document.querySelector(".button");
 
     button.addEventListener("click", (eve) => {
         CreateAddProductPage();
     });
+
+    table.addEventListener("click", (eve) => {
+
+        if (eve.target.classList.contains("updateProduct")) {
+            api(`https://localhost:7040/api/v1/Product/id/${eve.target.textContent}`).then(res => {
+                return res.json();
+            }).then(data => {
+                console.log(data);
+
+                let product = {
+                    name: data.name,
+                    price: data.price,
+                    stock: data.stock,
+                    producer: data.producer
+                }
+
+                CreateUpdatePage(product, eve.target.textContent);
+
+            }).catch(error => {
+                console.error('Error fetching data:', error);
+            });
+        }
+
+    });
+
+    if (alert === "deleted") {
+        load.classList = "";
+        appendAlert("Product has been DELETED with success!", "success");
+    }
+
+    if (alert === "updated") {
+        load.classList = "";
+        appendAlert("Product has been UPDATED with success!", "success");
+    }
+
+    if (alert === "added") {
+        load.classList = "";
+        appendAlert("Product has been ADDED with success!", "success");
+    }
 
 }
 
@@ -84,12 +143,90 @@ export function CreateAddProductPage() {
     let test = document.querySelector(".createProduct");
 
     button.addEventListener("click", (eve) => {
-        createHome();
+        createHome("");
     })
 
     test.addEventListener("click", (eve) => {
-        createProduct();
+        createUpdateProduct("create");
     })
+
+}
+
+export function CreateUpdatePage(product, idProduct) {
+
+    let container = document.querySelector(".container");
+
+    container.innerHTML = `
+    <h1>Update Doctor</h1>
+    <form>
+        <p>
+            <label for="name">Name</label>
+            <input name="name" type="text" id="name" value="${product.name}">
+             <a class="nameErr">Name required!</a>
+        </p>
+        <p>
+            <label for="price">Price</label>
+            <input name="price" type="text" id="price" value="${product.price}">
+             <a class="priceErr">Price required!</a>
+        </p>
+        <p>
+            <label for="stock">Stock</label>
+            <input name="stock" type="text" id="stock" value="${product.stock}">
+             <a class="stockErr">Stock required!</a>
+        </p>
+         <p>
+            <label for="producer">Producer</label>
+            <input name="producer" type="text" id="producer" value="${product.producer}">
+             <a class="producerErr">Producer required!</a>
+        </p>
+
+        <div class="submitUpdate">
+         <a href="#">Update Product</a>
+        </div>
+
+          <div class="cancel">
+         <a href="#">Cancel</a>
+        </div>
+        <div class="submitDelete">
+         <a href="#">Delete Product</a>
+        </div>
+    </form>
+    `
+
+    let cancelButton = document.querySelector(".cancel");
+    let submitUpdateButton = document.querySelector(".submitUpdate");
+    let submitDeleteButton = document.querySelector(".submitDelete");
+    let nameinput = document.getElementById("name");
+    let producerinput = document.getElementById("producer");
+
+    nameinput.disabled = true;
+    producerinput.disabled = true;
+
+    cancelButton.addEventListener("click", (eve) => {
+        createHome("");
+    });
+
+    submitUpdateButton.addEventListener("click", (eve) => {
+        createUpdateProduct("update", idProduct);
+    });
+
+    submitDeleteButton.addEventListener("click", (eve) => {
+
+        api(`https://localhost:7040/api/v1/Product/delete/${idProduct}`, "DELETE")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+
+                createHome("deleted");
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+
+    })
+
 
 }
 
@@ -98,7 +235,7 @@ function createRow(product) {
     let tr = document.createElement("tr");
 
     tr.innerHTML = `
-				<td>${product.id}</td>
+				<td class="updateProduct">${product.id}</td>
 				<td>${product.name}</td>
 				<td>${product.price}</td>
 				<td>${product.stock}</td>
@@ -140,7 +277,7 @@ function attachProducts(products) {
 
 }
 
-function createProduct() {
+function createUpdateProduct(request, idProduct) {
 
     const isNumber = (str) => {
         return /^[+-]?\d+(\.\d+)?$/.test(str);
@@ -237,17 +374,31 @@ function createProduct() {
             producer: producer
         }
 
-        api("https://localhost:7040/api/v1/Product/create", "POST", product)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                createHome();
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+        if (request === "create") {
+            api("https://localhost:7040/api/v1/Product/create", "POST", product)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome("added");
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        } else if (request === "update") {
+            api(`https://localhost:7040/api/v1/Product/update/${idProduct}`, "PUT", product)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    createHome("updated");
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
     } else {
 
         errors.forEach(err => {
